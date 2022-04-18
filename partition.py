bc90c8733c11845604de0eb0fc826051303b70e6
Jamin Liu <jaminliu@college.harvard.edu>
2022-04-18 19:07:31 -0400

    down bad

--- partition.py
+++ partition.py
@@ -1,1 +1,9 @@
+import random
+A = [10, 8, 7, 6, 5]
 
+generate_random_solution(A):
+    S = []
+    for i in len(A):
+        sign = 1 if random.random() < 0.5 else -1
+        S.append(sign)
+    return S
